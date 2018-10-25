d6b7a7150eb9769aefa0f24ce4025094d3579abe
עותמן <otma.ctos.asslan@hotmail.com>
2018-10-25 12:56:24 +0300

    1.splash screen is ok.......................

--- app/src/main/java/com/othman/ourmarket/LoginActivity.java
+++ app/src/main/java/com/othman/ourmarket/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.othman.ourmarket.taskFragments.dummy.SignUp;
 
@@ -12,12 +13,16 @@ public class LoginActivity extends AppCompatActivity {
 
 
     private Button signUp,logIn;
+    private EditText email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        signUp=findViewById(R.id.signUp);
+        signUp=(Button)findViewById(R.id.signUp);
+        logIn=(Button)findViewById(R.id.login);
+        email=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/othman/ourmarket/taskFragments/dummy/SignUp.java
+++ app/src/main/java/com/othman/ourmarket/taskFragments/dummy/SignUp.java
@@ -30,63 +30,99 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_SinUp);
-        firstName=findViewById(R.id.firstName);
-        LastName=findViewById(R.id.Lastname);
-        Email=findViewById(R.id.Email);
-        Password=findViewById(R.id.password);
-        save=findViewById(R.id.save);
-        auth=FirebaseAuth.getInstance();
-        user=auth.getCurrentUser();//return if user signed in if user didn't sign in  return null
+        firstName=(EditText)findViewById(R.id.firstName);
+        LastName=(EditText)findViewById(R.id.Lastname);
+        Email=(EditText)findViewById(R.id.Email);
+        Password=(EditText)findViewById(R.id.password);
+        save=(Button)findViewById(R.id.save);
+        auth=(FirebaseAuth)FirebaseAuth.getInstance();
+        user=(FirebaseUser)auth.getCurrentUser();//return if user signed in if user didn't sign in  return null
 
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean x=false;
-                if (Email!=null&&Password!=null){
-                  x=true;
-                    Intent i=new Intent(SignUp.this, LoginActivity.class);
+                dataHandler();
 
-                }
-                else{
-                    x=false;
-                    Toast.makeText(SignUp.this, "failed to sign up please enter the current email and password", Toast.LENGTH_SHORT).show();
-                }
-                if (x==true){
-                    auth.createUserWithEmailAndPassword(Email.getText().toString(), Password.getText().toString()).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){//chek if response arrive to server
-                                Toast.makeText(SignUp.this, "Auth notification Successful", Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-                            else {
-                                Toast.makeText(SignUp.this, "Auth notificatio Failed "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                task.getException().printStackTrace();
-                            }
-                        }
-                    });
-                }
 
+
+//                boolean x=false;
+//                if (Email!=null&&Password!=null){
+//                  x=true;
+//                    Intent i=new Intent(SignUp.this, LoginActivity.class);
+//
+//                }
+//                else{
+//                    x=false;
+//                    Toast.makeText(SignUp.this, "failed to sign up please enter the current email and password", Toast.LENGTH_SHORT).show();
+//                }
+//                if (x==true){
+//                    auth.createUserWithEmailAndPassword(Email.getText().toString(), Password.getText().toString()).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if (task.isSuccessful()){//chek if response arrive to server
+//                                Toast.makeText(SignUp.this, "Auth notification Successful", Toast.LENGTH_SHORT).show();
+//                                finish();
+//                            }
+//                            else {
+//                                Toast.makeText(SignUp.this, "Auth notificatio Failed "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                                task.getException().printStackTrace();
+//                            }
+//                        }
+//                    });
+//                }
+//                else{
+//                    x=false;
+//                    Toast.makeText(SignUp.this, "failed to sign up", Toast.LENGTH_SHORT).show();
+//                }
+//
 
             }
         });
 
     }
-//    private void creatAcount(String email, String password){
-//        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
-//            @Override
-//            public void onComplete(@NonNull Task<AuthResult> task) {
-//               if (task.isSuccessful()){//chek if response arrive to server
-//                   Toast.makeText(SignUp.this, "Auth notification Successful", Toast.LENGTH_SHORT).show();
-//                   finish();
-//               }
-//               else {
-//                   Toast.makeText(SignUp.this, "Auth notificatio Failed "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-//                   task.getException().printStackTrace();
-//               }
-//            }
-//        });
-//    }
+    /**
+     * get email and password from the field and try to create new user
+     *
+     */
+    private void dataHandler() {
+
+        boolean isk=true;//if all the fields filled will
+        String email=Email.getText().toString();
+        String password=Password.getText().toString();
+        String firstname=firstName.getText().toString();
+        String lastname=LastName.getText().toString();
+        if(email.length()>8||email.indexOf('@')<0||email.indexOf('.')<0){
+            Email.setError("Wrong email");
+            isk=false;
+        }
+        if (password.length()<8){
+            isk=false;
+        }
+        if (isk){
+            creatAcount(email,password);
+        }
+
+
+    }
+    /**
+     * create firebase using email and password
+     */
+    private void creatAcount(String email, String password){
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+          @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+               if (task.isSuccessful()){//chek if response arrive to server
+                   Toast.makeText(SignUp.this, "Auth notification Successful", Toast.LENGTH_SHORT).show();
+                   finish();
+               }
+              else {
+                   Toast.makeText(SignUp.this, "Auth notificatio Failed "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                  task.getException().printStackTrace();
+               }
+           }
+        });
+    }
 
 }
